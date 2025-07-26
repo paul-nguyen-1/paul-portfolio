733e03a2c5e71c5ea012580e2cb3173eb0783fd6
paul-nguyen96 <paul121596@icloud.com>
2025-07-26 12:01:47 -0500

    setup tanstack

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,16 @@ import "./index.css";
 import App from "./App.tsx";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider>
-      <App />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <App />
+      </MantineProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
