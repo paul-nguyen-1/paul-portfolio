a0b4d35779df85a8cdbfbcd2b58b825bfe8b46da
paul-nguyen96 <paul121596@icloud.com>
2025-07-26 12:01:26 -0500

    enable cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      process.env.ALFRED_BACKEND_ENDPOINT,
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
