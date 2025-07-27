1b3aa384cc8c223463bd7ff729912061717f0d05
paul-nguyen96 <paul121596@icloud.com>
2025-07-27 01:56:46 -0500

    install vercel analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App.tsx";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <MantineProvider>
+        <Analytics />
         <App />
       </MantineProvider>
     </QueryClientProvider>
