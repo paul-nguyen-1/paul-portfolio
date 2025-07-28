58752231a3e86f747329e46771a4bbd45b7aeb9f
paul-nguyen96 <paul121596@icloud.com>
2025-07-28 18:52:37 -0500

    align center

--- client/src/components/chat/header.tsx
+++ client/src/components/chat/header.tsx
@@ -127,7 +127,7 @@ export function Header() {
       </Card>
 
       <div className="flex justify-center mt-4 rounded-lg shadow-lg text-white">
-        <div className="h-16 flex justify-center px-4 py-2 w-11/12 bg-gray-700 border border-gray-600 rounded-lg">
+        <div className="h-16 flex items-center justify-center px-4 py-2 w-11/12 bg-gray-700 border border-gray-600 rounded-lg">
           <AnimatePresence mode="wait">
             <a
               href={link}
