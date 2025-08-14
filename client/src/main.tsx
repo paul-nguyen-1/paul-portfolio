47863ff54423594b1921f43f0102798acce23244
paul-nguyen96 <paul121596@icloud.com>
2025-08-14 04:51:12 -0500

    set up tanstack routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,16 @@ import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen.ts";
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const router = createRouter({ routeTree });
 
 const queryClient = new QueryClient();
 
@@ -13,8 +23,8 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <MantineProvider>
+        <RouterProvider router={router} />
         <Analytics />
-        <App />
       </MantineProvider>
     </QueryClientProvider>
   </StrictMode>
